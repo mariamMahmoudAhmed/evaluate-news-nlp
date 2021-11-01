c6dc7718833dbfe66f17ba98fe6ae0d3495b5f60
mariamMahmoudAhmed <92438056+mariamMahmoudAhmed@users.noreply.github.com>
2021-11-01 13:51:23 +0200

    Create formHandler.test.js

---
+++ __test__/formHandler.test.js
@@ -0,0 +1,6 @@
+const handleSubmit = require("../src/client/js/formHandler")
+describe("Testing functionality", () => {
+        test("Testing handleSubmit() function", () => {
+               expect(handleSubmit).toBeDefined()
+});
+});
